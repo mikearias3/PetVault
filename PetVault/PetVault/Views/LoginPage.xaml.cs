965fdec3cb880712e395d695b78298b8ce747f84
Mike Arias <michael.arias@techaid.com>
2017-07-23 11:57:01 -0400

    Initial commit and visual implementation of LoginPage

---
+++ PetVault/PetVault/Views/LoginPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PetVault.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+        }
+    }
+}
